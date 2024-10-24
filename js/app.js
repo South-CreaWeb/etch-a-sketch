a09c3106b99502efdb2e9f08a0704c3dff189685
South-CreaWeb <south-creaweb@outlook.fr>
2024-10-24 17:29:00 +0200

    Improve logics

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const divs = document.querySelector(".container");
+const askButton = document.querySelector(".button__ask");
+
+let isDefaultGridDisable = false;
 
 /**
  *
@@ -12,25 +15,48 @@ function createElement(tagname) {
 }
 
 /**
- * Create a 16x16 grid of square
+ * Create a 16x16 square grid
  */
-function createGrid() {
-  let div = null;
-  for (let i = 0; i <= 255; i++) {
-    div = createElement("div");
-
+function createDefaultGrid(a) {
+  let total = a * a;
+  console.log("la");
+  if (isDefaultGridDisable) return;
+  for (let i = 1; i <= total; i++) {
+    let div = createElement("div");
     div.classList.add("square");
     divs.appendChild(div);
 
     div.addEventListener("mouseenter", (event) => {
       let currentDiv = event.currentTarget;
-      currentDiv.style["background-color"] = "black";
+      currentDiv.style["background-color"] = "darkgray";
       setTimeout(() => {
-        currentDiv.style["background-color"] = "cyan";
+        currentDiv.removeAttribute("style");
       }, 1500);
     });
   }
-  return div;
 }
 
-createGrid();
+/**
+ * Ask the user how many suqare he want and generate it
+ * @param {number} number
+ */
+function askNumberSquare(number) {
+  number = prompt("entrez un chiffre");
+  const resultGrid = number * number;
+
+  if (number >= 100) {
+    return alert("Nombre beaucoup trop grand");
+  } else {
+    isDefaultGridDisable = true;
+    const customDiv = divs;
+    divs.remove();
+    for (let i = 0; i <= resultGrid; i++) {}
+  }
+}
+
+askButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  askNumberSquare();
+});
+
+createDefaultGrid(16);
